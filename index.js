f43ab09e36d3c87ce98d09f2133f6baaecde9c04
zulianAdryan <zulian.adryan@gmail.com>
2023-08-20 18:21:39 +0700

    fix port

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
